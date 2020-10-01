186d345c9ced5c37841141d6f64df4d25fcc4882
Matt Andrews <mlandrews.hs.ga@gmail.com>
2020-10-01 16:41:50 -0400

    Additional commits (forgot to make on previous commit).

--- example_client.js
+++ example_client.js
@@ -1,25 +1,19 @@
 "use strict";
 const ModbusTCP = require("./src/modbustcp.js");
 
-let client = new ModbusTCP().Client();
+let modbusTCP = new ModbusTCP();
+let client = new modbusTCP.client();
 
-let query = {
-    type: "readHoldingRegisters",
-    address: 0,
-    length: 1
-};
-
-client.connect("127.0.0.1", 502, connected);
-
-function connected () {
-    client.sendQuery(query, reply);
-}
-
-function reply (data) {
-    console.log(data);
-    setTimeout(function () {
-        query.address = Math.round(Math.random() * 65000);
-        query.length = Math.round(Math.random() * 30);
-        client.sendQuery(query, reply);
-    }, 33);
-}
+client.connect({ port: 502, ip: '192.168.1.71', reconnect: false }, (err) => {
+    if (err) { console.error(err); }
+    else {
+        console.log("Connected");
+        client.readHoldingRegisters(1,0,1, (err, data) => {
+            if (err) { console.error(err.message); }
+            else { console.log(data); }
+            client.disconnect(() => {
+                console.log("Disconnected");
+            });
+        });
+    }
+});
