fdefeeab4c5838d981f8efbfc33f0a88f324e6a1
Matt Andrews <andrews_matthew_l@cat.com>
2022-04-20 17:21:48 -0400

    Now supports writeHoldingRegisters

--- src/modbus.js
+++ src/modbus.js
@@ -33,10 +33,29 @@ module.exports = function () {
             }
         }
         else if (this.functionCode == 16) { // writeHoldingRegisters
-
+            if (buffer.length >= 15) { // request
+                this.type = "writeHoldingRegistersRequest";
+                this.address = buffer.readUInt16BE(8);
+                this.numAddresses = buffer.readUInt16BE(10);
+                this.dataLength = buffer.readUInt8(12);
+                if (this.dataLength > 240) { throw new Error("readHoldingRegisters: response too long"); return; }
+                if ((this.dataLength % 2) !== 0) { throw new Error("readHoldingRegisters: data length cannot be odd"); return; }
+                this.data = [];
+                for (let i=0; i< this.dataLength; i+=2) {
+                    this.data.push(buffer.readUInt16BE(13 + i));
+                }
+            }
+            else { // response
+                this.type = "writeHoldingRegistersResponse";
+                this.address = buffer.readUInt16BE(8);
+                this.numAddresses = buffer.readUInt16BE(10);
+            }
         }
         else if (this.functionCode == 131) { // readHoldingRegisters exception
             
+        }
+        else if (this.functionCode == 144) { // writeHoldingRegisters exception
+
         }
         else { throw new Error("not recoginized"); }
     }
@@ -49,12 +68,25 @@ module.exports = function () {
             buffer.writeUInt16BE(this.address, 8);
             buffer.writeUInt16BE(this.numAddresses, 10);
         }
-        if (this.type === "readHoldingRegistersResponse") {
+        else if (this.type === "readHoldingRegistersResponse") {
             buffer.writeUInt8(this.dataLength, 8);
             for (let i=0; i<this.data.length; i++) {
                 buffer.writeUInt16BE(this.data[i], (9 + (i * 2)));
             }
         }
+        else if (this.type === "writeHoldingRegistersRequest") {
+            buffer.writeUInt16BE(this.address, 8);
+            buffer.writeUInt16BE(this.numAddresses, 10);
+            buffer.writeUInt8(this.dataLength, 12);
+            for (let i=0; i<this.data.length; i++) {
+                buffer.writeUInt16BE(this.data[i], (13 + (i * 2)));
+            }
+        }
+        else if (this.type === "writeHoldingRegistersResponse") {
+            buffer.writeUInt16BE(this.address, 8);
+            buffer.writeUInt16BE(this.numAddresses, 10);
+        }
+
         return buffer;
     }
 
--- test/test_modbusTCP.js
+++ test/test_modbusTCP.js
@@ -14,7 +14,7 @@ describe("modbus", function () {
             assert.strictEqual(modbus.functionCode, null);
             assert.strictEqual(modbus.type, null);
         });
-        it("should return a valid Read Holding Register Request from buffer", function () {
+        it("should return a valid Read Holding Registers Request from buffer", function () {
             let modbus = new MODBUS();
             modbus.fromBuffer(Buffer.from([0,1,0,0,0,6,2,3,0,4,0,5]));
             assert.strictEqual(modbus.mbap.transaction, 1);
@@ -26,7 +26,7 @@ describe("modbus", function () {
             assert.strictEqual(modbus.address, 4);
             assert.strictEqual(modbus.numAddresses, 5);
         });
-        it("should return a valid Read Holding Register Response from buffer", function () {
+        it("should return a valid Read Holding Registers Response from buffer", function () {
             let modbus = new MODBUS();
             modbus.fromBuffer(Buffer.from([0,6,0,0,0,9,7,3,6,0,8,9,0,0,10]));
             assert.strictEqual(modbus.mbap.transaction, 6);
@@ -38,7 +38,33 @@ describe("modbus", function () {
             assert.strictEqual(modbus.dataLength, 6);
             assert.deepStrictEqual(modbus.data, [8, 2304, 10]);
         });
-        it("should return a valid buffer from a Read Holding Register Request", function () {
+        it("should return a valid Write Holding Registers Request from buffer", function () {
+            let modbus = new MODBUS();
+            modbus.fromBuffer(Buffer.from([0,1,0,0,0,11,2,16,0,3,0,4,8,0,9,0,10,0,11,0,12]));
+            assert.strictEqual(modbus.mbap.transaction, 1);
+            assert.strictEqual(modbus.mbap.protocol, 0);
+            assert.strictEqual(modbus.mbap.byteLength, 11);
+            assert.strictEqual(modbus.device, 2);
+            assert.strictEqual(modbus.functionCode, 16);
+            assert.strictEqual(modbus.type, "writeHoldingRegistersRequest");
+            assert.strictEqual(modbus.address, 3);
+            assert.strictEqual(modbus.numAddresses, 4);
+            assert.strictEqual(modbus.dataLength, 8);
+            assert.deepStrictEqual(modbus.data, [9, 10, 11, 12]);
+        });
+        it("should return a valid Write Holding Registers Response from buffer", function () {
+            let modbus = new MODBUS();
+            modbus.fromBuffer(Buffer.from([0,13,0,0,0,9,14,16,0,15,0,16]));
+            assert.strictEqual(modbus.mbap.transaction, 13);
+            assert.strictEqual(modbus.mbap.protocol, 0);
+            assert.strictEqual(modbus.mbap.byteLength, 9);
+            assert.strictEqual(modbus.device, 14);
+            assert.strictEqual(modbus.functionCode, 16);
+            assert.strictEqual(modbus.type, "writeHoldingRegistersResponse");
+            assert.strictEqual(modbus.address, 15);
+            assert.strictEqual(modbus.numAddresses, 16);
+        });
+        it("should return a valid buffer from a Read Holding Registers Request", function () {
             let modbus = new MODBUS();
             modbus.mbap.transaction = 11;
             modbus.mbap.protocol = 12;
@@ -50,7 +76,7 @@ describe("modbus", function () {
             modbus.numAddresses = 15;
             assert.deepStrictEqual(modbus.toBuffer(),Buffer.from([0,11,0,12,0,6,13,3,0,14,0,15]));
         });
-        it("should return a valid buffer from a Read Holding Register Request", function () {
+        it("should return a valid buffer from a Read Holding Registers Request", function () {
             let modbus = new MODBUS();
             modbus.mbap.transaction = 16;
             modbus.mbap.protocol = 17;
