a8adf87334eda48e6b93c95690e68c1949db9363
Matt Andrews <andrews_matthew_l@cat.com>
2023-07-05 17:09:05 -0400

    Added client operations, and a basic implementation.

--- basicClient.js
+++ basicClient.js
@@ -5,13 +5,16 @@ const MBCLIENT = require("./src/modbusTCPClient.js");
 let mbClient = new MBCLIENT();
 
 mbClient.host = "127.0.0.1";
-mbClient.port = 502;
+mbClient.port = 50200;
 
-mbClient.readHoldingRegisters(1,0,1)
-    .then((data) => {
-        console.log(data);
-        process.exit(0);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+let transaction = 0;
+
+function query_00 () {
+    if (! mbClient.awaitingReply) {
+        mbClient.readHoldingRegisters(transaction++, 1, 0, 3)
+            .then((data) => { console.log(data); })
+            .catch((err) => { console.log(err); });
+    }
+}
+
+setInterval(query_00, 100);
